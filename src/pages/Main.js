95a661e706baa6bee340c2930338659514b4f11a
Sharathsajithaiparambil <117640487+Sharathsajithaiparambil@users.noreply.github.com>
2024-08-16 14:30:38 +0530

    Update Main.js

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,31 +1,25 @@
-import React from 'react'
-import {Container,Nav,Navbar} from 'react-bootstrap';
-
-
-
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 const Main = () => {
   return (
     <div>
-
-<>
-      <Navbar bg="light" data-bs-theme="light">
+      <Navbar bg="light" expand="lg">
         <Container>
           <Navbar.Brand href="/">SHARATH SAJI</Navbar.Brand>
-          <Nav className="float-end">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/services">Services</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
-
-          </Nav>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/about">About</Nav.Link>
+              <Nav.Link href="/services">Services</Nav.Link>
+              <Nav.Link href="/contact">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
-
-    </>
-
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
